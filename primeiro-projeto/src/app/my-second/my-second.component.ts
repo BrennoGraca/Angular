b0cab7f6b9d098b3d722e790a3b62d6280888c58
Brenno Graça <brennosg_3712@hotmail.com>
2022-09-21 12:44:23 -0300

    Creating components

---
+++ primeiro-projeto/src/app/my-second/my-second.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-second',
+  templateUrl: './my-second.component.html',
+  styleUrls: ['./my-second.component.css']
+})
+export class MySecondComponent {
+
+  
+
+}
